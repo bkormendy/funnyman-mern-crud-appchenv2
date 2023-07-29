44031c62355d4ea2157fa21f71b5b4ee44a35096
Boldizsar <boldizsar.kormendy@gmail.com>
2023-07-29 15:26:52 +0200

    Added test for post method

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,17 @@ class MessageApp {
     constructor() {
         this.messages = []
     }
+
+    post(content) {
+        let item = {
+            id: this.messages.length,
+            content: content,
+            date: new Date()
+        }
+        this.messages.push(item)
+        return this.messages
+    }
+    
 }
 
 export default MessageApp
--- Backend/app.test.js
+++ Backend/app.test.js
@@ -4,7 +4,19 @@ import MessageApp from './app.js'
 
 describe("app", function() {
     let testApp = new MessageApp
+
+    beforeEach(()=>{
+        testApp = new MessageApp
+        testApp.post('hi world')
+    })
+
     it("app has messages", function() {
     expect(testApp.messages).to.be.an('array')
-  })
+    })
+
+    it("app creates message (post)", function() {
+        testApp.post('hi world')
+        expect(testApp.messages.length).to.equal(2)
+    })
+
 })
