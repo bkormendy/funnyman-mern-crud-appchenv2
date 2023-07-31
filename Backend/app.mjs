6684e3d60b3b0eb5232d36b03bc11645364812aa
Boldizsar <boldizsar.kormendy@gmail.com>
2023-07-31 15:23:12 +0200

    Added ENV (cross env) to divide messages.json and testMessages.json.

--- Backend/app.mjs
+++ Backend/app.mjs
@@ -7,6 +7,12 @@ const app = express()
 //hardcoded instance of message app test database vv
 let messageApp = new MessageApp("/\///json/\//testMessages.json")
 
+if (process.env.npm_lifecycle_event == "test") {
+   messageApp = new MessageApp(`/\///json/\//testMessages.json`)
+   } else {
+      messageApp = new MessageApp(`/\///json/\//messages.json`)
+   }
+
 app.get('/', async (req, res) => {
    return new Promise((resolve, reject) => {
      let result = messageApp.getAll()
